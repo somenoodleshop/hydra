e6073e5371e89323cbc0c87386584da79cc877ab
arandombit <a.random.luis@proton.me>
2025-05-02 22:26:44 -0400

    add sidebar with chat sessions that one can select from

--- src/containers/Chat.tsx
+++ src/containers/Chat.tsx
@@ -1,32 +1,46 @@
 import { useState } from 'react'
 import { View, ScrollView } from 'react-native'
 
+import mockData from '~/mock/data.json'
+
+import useLocalStorage from '~/hook/useLocalStorage'
 import { sendMessage } from '~/handler/chat'
 
 import { Input } from '~/reusables/ui/input'
 
 import ChatMessage from '~/components/ChatMessage'
+import Sessions from '~/containers/Sessions'
 
 const Chat = () => {
+  const [currentSession, setCurrentSession] = useState(mockData.sessions[0])
+  const [sessions, setSessions] = useLocalStorage('sessions', mockData.sessions)
   const [message, setMessage] = useState('')
-  const [messages, setMessages] = useState([])
+  const [messages, setMessages] = useState(mockData.messages[currentSession.id])
 
   return (
-    <View className='flex-1 w-full bg-background'>
-      <ScrollView className='flex-1 px-4 pt-4'>
-        { messages.map(msg => (<ChatMessage key={msg.id} {...msg} />)) }
-      </ScrollView>
-      <View className='flex-row items-center gap-2 p-4 border-t border-border'>
-        <Input
-          autoFocus
-          inputMode='text'
-          className='flex-1'
-          value={message}
-          onChangeText={setMessage}
-          placeholder="Type a message..."
-          onSubmitEditing={sendMessage({ message, setMessage, setMessages })}
-          returnKeyType="send"
+    <View className='flex-1 w-full bg-background flex-row'>
+      <View style={{ width: '15%' }} className='h-full border-r border-border px-2 pt-4'>
+        <Sessions
+          sessions={sessions}
+          onSessionSelect={id => setCurrentSession(id)}
         />
+      </View>
+      <View style={{ width: '85%' }} className='h-full flex-1'>
+        <ScrollView className='flex-1 px-4 pt-4'>
+          { messages.map(msg => (<ChatMessage key={msg.id} {...msg} />)) }
+        </ScrollView>
+        <View className='flex-row items-center gap-2 p-4 border-t border-border'>
+          <Input
+            autoFocus
+            inputMode='text'
+            className='flex-1'
+            value={message}
+            onChangeText={setMessage}
+            placeholder="Type a message..."
+            onSubmitEditing={sendMessage({ message, setMessage, setMessages })}
+            returnKeyType="send"
+          />
+        </View>
       </View>
     </View>
   )
---
+++ src/containers/Sessions.js
@@ -0,0 +1,22 @@
+import { View } from 'react-native'
+
+import { Button } from '~/reusables/ui/button'
+import { Text } from '~/reusables/ui/text'
+
+const Sessions = props => {
+  return (
+    <View style={{ gap: 8 }}>
+      { props.sessions.map(session => (
+        <Button
+          key={session.id}
+          variant="link"
+          onPress={() => props.onSessionSelect(session.id)}
+        >
+          <Text className='text-sm'>{ session.name }</Text>
+        </Button>
+      )) }
+    </View>
+  )
+}
+
+export default Sessions
