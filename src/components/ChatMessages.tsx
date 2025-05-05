a25f98ab46ebf6e871806d16d6201ff51cc123f0
arandombit <a.random.luis@proton.me>
2025-05-04 22:55:57 -0400

    extract messages into separate stateful container component

---
+++ src/components/ChatMessages.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react'
+import { ScrollView, View } from 'react-native'
+
+import mockData from '~/mock/data.json'
+
+import { sendMessage } from '~/handler/chat'
+
+import { Input } from '~/reusables/ui/input'
+
+import ChatMessage from './ChatMessage'
+
+const ChatMessages = props => {
+  const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState(mockData.messages[props.currentSession])
+
+  useEffect(() => { setMessages(mockData.messages[props.currentSession]) }, [props.currentSession])
+
+  return (
+    <View className='h-full flex-1 w-[80%]'>
+      <ScrollView className='flex-1 px-4 pt-4'>
+        { messages.map(msg => (<ChatMessage key={msg.id} {...msg} />)) }
+      </ScrollView>
+      <View className='flex-row items-center gap-2 p-4 border-t border-border'>
+        <Input
+          autoFocus
+          inputMode='text'
+          className='flex-1'
+          value={message}
+          onChangeText={setMessage}
+          placeholder='Type a message...'
+          onSubmitEditing={sendMessage({ message, setMessage, setMessages })}
+          returnKeyType='send'
+        />
+      </View>
+    </View>
+  )
+}
+
+export default ChatMessages
--- src/containers/Chat.tsx
+++ src/containers/Chat.tsx
@@ -1,52 +1,23 @@
 import { useState } from 'react'
-import { View, ScrollView } from 'react-native'
+import { View } from 'react-native'
 
 import mockData from '~/mock/data.json'
 
 import usePersistentStorage from '~/hook/usePersistentStorage'
-import { sendMessage } from '~/handler/chat'
 
-import { Input } from '~/reusables/ui/input'
-
-import ChatMessage from '~/components/ChatMessage'
+import ChatMessages from '~/components/ChatMessages'
 import Sessions from '~/containers/Sessions'
-
-const handleSessionSelect = api => id => {
-  api.setCurrentSession(id)
-  api.setMessages(mockData.messages[id])
-}
 
 const Chat = () => {
   const [currentSession, setCurrentSession] = useState(mockData.sessions[0].id)
   const [sessions, setSessions] = usePersistentStorage('sessions', mockData.sessions)
-  const [message, setMessage] = useState('')
-  const [messages, setMessages] = useState(mockData.messages[currentSession])
 
   return (
     <View className='flex-1 w-full bg-background flex-row'>
       <View className='h-full border-r border-border px-2 pt-4 w-[20%]'>
-        <Sessions
-          sessions={sessions}
-          onSessionSelect={handleSessionSelect({ setCurrentSession, setMessages })}
-        />
+        <Sessions sessions={sessions} onSessionSelect={setCurrentSession} />
       </View>
-      <View className='h-full flex-1 w-[80%]'>
-        <ScrollView className='flex-1 px-4 pt-4'>
-          { messages.map(msg => (<ChatMessage key={msg.id} {...msg} />)) }
-        </ScrollView>
-        <View className='flex-row items-center gap-2 p-4 border-t border-border'>
-          <Input
-            autoFocus
-            inputMode='text'
-            className='flex-1'
-            value={message}
-            onChangeText={setMessage}
-            placeholder='Type a message...'
-            onSubmitEditing={sendMessage({ message, setMessage, setMessages })}
-            returnKeyType='send'
-          />
-        </View>
-      </View>
+      <ChatMessages {...{ currentSession }} />
     </View>
   )
 }
