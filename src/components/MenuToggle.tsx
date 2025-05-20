3d5257ac0691025539659525eee32f1e99aab701
arandombit <a.random.luis@proton.me>
2025-05-19 20:47:51 -0400

    add more styling to menu toggle to make it similar to theme toggle

--- src/components/MenuToggle.tsx
+++ src/components/MenuToggle.tsx
@@ -1,13 +1,24 @@
 import { DrawerActions } from '@react-navigation/native'
-import { Pressable } from 'react-native'
+import { Pressable, View } from 'react-native'
 
+import { cn } from '~/lib/utils'
 import { Menu } from 'lib/icons/Menu'
 
 const MenuToggle = props => (
   <Pressable
-    className='ml-3 web:ring-offset-background web:transition-colors web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2'
-    onPress={() => props.navigation.dispatch(DrawerActions.openDrawer())}>
-    <Menu className='text-foreground' size={24} strokeWidth={1.25} />
+    className='web:ring-offset-background web:transition-colors web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2'
+    onPress={() => props.navigation.dispatch(DrawerActions.openDrawer())}
+  >
+    {({ pressed }) => (
+      <View
+        className={cn(
+          'flex-1 aspect-square pt-0.5 justify-center items-start web:px-5',
+          pressed && 'opacity-70'
+        )}
+      >
+        <Menu className='text-foreground' size={24} strokeWidth={1.25} />
+      </View>
+    )}
   </Pressable>
 )
 
