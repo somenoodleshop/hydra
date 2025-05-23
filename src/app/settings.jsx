9005252b3ad356fd12c89689c29b2f0481e82352
arandombit <a.random.luis@proton.me>
2025-05-22 22:47:21 -0400

    add settings page

---
+++ src/app/settings.jsx
@@ -0,0 +1,10 @@
+import { View } from 'react-native';
+import { Text } from '~/reusables/ui/text';
+
+export default function Settings() {
+  return (
+    <View className='flex-1 p-4'>
+      <Text className='text-2xl font-bold mb-4'>Settings</Text>
+    </View>
+  )
+} 
