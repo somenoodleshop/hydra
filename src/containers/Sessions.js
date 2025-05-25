5a36b85fad429249d577ec963629bd2cc4a3f041
arandombit <a.random.luis@proton.me>
2025-05-24 22:32:56 -0400

    remove unnecessary indentation from component

--- src/containers/Sessions.js
+++ src/containers/Sessions.js
@@ -3,21 +3,19 @@ import { View } from 'react-native'
 import { Button } from '~/reusables/ui/button'
 import { Text } from '~/reusables/ui/text'
 
-const Sessions = props => {
-  return (
-    <View style={{ gap: 8 }}>
-      { props.sessions.map(session => (
-        <Button
-          key={session.id}
-          variant='link'
-          onPress={() => props.onSessionSelect(session.id)}
-          className='w-full items-start'
-        >
-          <Text className='text-sm'>{ session.name }</Text>
-        </Button>
-      )) }
-    </View>
-  )
-}
+const Sessions = props => (
+  <View style={{ gap: 8 }}>
+    { props.sessions.map(session => (
+      <Button
+        key={session.id}
+        variant='link'
+        onPress={() => props.onSessionSelect(session.id)}
+        className='w-full items-start'
+      >
+        <Text className='text-sm'>{ session.name }</Text>
+      </Button>
+    )) }
+  </View>
+)
 
 export default Sessions
