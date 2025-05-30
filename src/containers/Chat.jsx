48248750f48053f3d9b742eb9f6b6846841d4046
arandombit <a.random.luis@proton.me>
2025-05-30 07:37:58 -0400

    fetch messages from API

--- src/containers/Chat.jsx
+++ src/containers/Chat.jsx
@@ -1,18 +1,22 @@
 import { useState, useEffect } from 'react'
-import { Platform, View } from 'react-native'
+import { Platform, View, Text } from 'react-native'
 import { useRoute } from '@react-navigation/native'
+import { useQuery } from '@tanstack/react-query'
 
-import mockData from '~/mock/data.json'
+import mockData from 'mock/data.json'
 
-import usePersistentStorage from '~/hook/usePersistentStorage'
+import usePersistentStorage from 'hook/usePersistentStorage'
+import { getMessages } from 'query/chat'
 
-import ChatMessages from '~/containers/ChatMessages'
-import Sessions from '~/containers/Sessions'
+import ChatMessages from 'containers/ChatMessages'
+import Sessions from 'containers/Sessions'
 
 const Chat = () => {
   const route = useRoute()
   const [currentSession, setCurrentSession] = useState(mockData.sessions[0].id)
   const [sessions, setSessions] = usePersistentStorage('sessions', mockData.sessions)
+
+  const { data = { messages: [] }, isLoading } = useQuery(getMessages)
 
   useEffect(() => {
     if (route.params?.sessionId) {
@@ -27,7 +31,12 @@ const Chat = () => {
           <Sessions sessions={sessions} onSessionSelect={setCurrentSession} />
         </View>
       )}
-      <ChatMessages {...{ currentSession }} />
+      { !isLoading
+        ? <ChatMessages currentSession={currentSession} messages={data.messages} />
+        : <View className='h-full flex-1 w-[80%] items-center justify-center'>
+            <Text>Loading messages...</Text>
+          </View>
+      }
     </View>
   )
 }
--- src/containers/ChatMessages.jsx
+++ src/containers/ChatMessages.jsx
@@ -20,11 +20,11 @@ const ChatMessages = props => {
       <ScrollView className='flex-1 px-4 pt-4'>
         { messages.map(msg => (<Message key={msg.id} {...msg} />)) }
       </ScrollView>
-      <View className='flex-row items-center gap-2 p-4 border-t border-border'>
+      <View className='flex-row w-[50%] items-center gap-2 p-4 border-t border-border'>
         <Input
           autoFocus
           inputMode='text'
-          className='flex-1'
+          className='flex-1 w-[50%]'
           value={message}
           onChangeText={setMessage}
           placeholder='Type a message...'
