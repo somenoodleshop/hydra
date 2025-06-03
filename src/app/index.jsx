bfba8f6261e6cc9d2814a62b24e20ced6d475879
arandombit <a.random.luis@proton.me>
2025-06-02 20:17:06 -0400

    remove redundant CSS class

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -4,7 +4,7 @@ import Chat from '~/containers/Chat'
 
 export default function Screen() {
   return (
-    <View className='w-full flex-1 w-full justify-center items-center gap-5 p-6'>
+    <View className='w-full flex-1 justify-center items-center gap-5 p-6'>
       <Chat />
     </View>
   )
