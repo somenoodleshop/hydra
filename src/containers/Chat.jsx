fdd1630044ad7ea6e346de00efcebf04d9a9f8df
arandombit <a.random.luis@proton.me>
2025-07-13 22:52:26 +0200

    add some preliminary code for creating new sessions with actual data

--- src/containers/Chat.jsx
+++ src/containers/Chat.jsx
@@ -4,9 +4,18 @@ import { useNavigation, useRoute } from '@react-navigation/native'
 
 import usePersistentStorage from '~/hook/usePersistentStorage'
 
+import { Button } from '~/reusables/ui/button'
+import { Text } from '~/reusables/ui/text'
+
 import ChatMessages from '~/containers/ChatMessages'
 import Sessions from '~/containers/Sessions'
 import SettingsButton from '~/components/SettingsButton'
+
+const createNewSession = ({ setSessions, setCurrentSession }) => () => {
+  const session = { id: Date.now().toString(), name: 'Untitled Chat' }
+  setSessions([...sessions, session])
+  setCurrentSession(session.id)
+}
 
 const Chat = ()=> {
   const navigation = useNavigation()
@@ -21,14 +30,25 @@ const Chat = ()=> {
   }, [route.params?.sessionId])
 
   return (
-    <View className='flex-1 w-full bg-background flex-row'>
-      { Platform.OS === 'web' && sessions.length > 0 && (
-        <View className='h-full border-r border-border px-2 pt-4 w-[20%]'>
-          <Sessions sessions={sessions} onSessionSelect={setCurrentSession} />
-          <SettingsButton onPress={() => navigation.navigate('settings')} />
-        </View>
-      ) }
-      <ChatMessages currentSession={currentSession} noSessions={sessions.length === 0} />
+    <View className='flex-1 w-full bg-background'>
+      <View className='flex-1 w-full h-[5%] justify-end'>
+        <Button
+          variant='outline'
+          className='mb-4 mt-4 w-[5%]'
+          onPress={createNewSession({ setSessions, setCurrentSession })}
+        >
+          <Text>New Chat</Text>
+        </Button>
+      </View>
+      <View className='flex-1 w-full bg-background flex-row'>
+        { Platform.OS === 'web' && sessions.length > 0 && (
+          <View className='h-full border-r border-border px-2 pt-4 w-[20%]'>
+            <Sessions sessions={sessions} onSessionSelect={setCurrentSession} />
+            <SettingsButton onPress={() => navigation.navigate('settings')} />
+          </View>
+        ) }
+        <ChatMessages currentSession={currentSession} noSessions={sessions.length === 0} />
+      </View>
     </View>
   )
 }
