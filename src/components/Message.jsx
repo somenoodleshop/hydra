55863f5c13b6db3640bd0bc8d59e38d8179221f6
arandombit <a.random.luis@proton.me>
2025-07-28 23:21:19 +0200

    remove broken styling for chat messages

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,11 +3,7 @@ import { View } from 'react-native'
 import { Text } from '~/reusables/ui/text'
 
 const Message = props => (
-  <View 
-    className={`flex-1 items-center max-w-[80%] px-4 py-2 mb-2 bg-primary/70 rounded-2xl ${
-      props.role === 'user' ? 'justify-end rounded-br-sm' : 'justify-start rounded-bl-sm'
-    }`}
-  >
+  <View>
     <Text className='text-background'>
       { props.content }
     </Text>
