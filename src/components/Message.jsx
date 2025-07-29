c4be88c5746383c16de7e60c24d3dfa1de3d817e
arandombit <a.random.luis@proton.me>
2025-07-29 22:14:40 +0200

    fix styling issues with messages being displayed

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,10 +3,8 @@ import { View } from 'react-native'
 import { Text } from '~/reusables/ui/text'
 
 const Message = props => (
-  <View>
-    <Text className='text-background'>
-      { props.content }
-    </Text>
+  <View className='items-center max-w-[80%] px-4 py-2 mb-2 rounded-2xl'>
+    <Text>{ props.content }</Text>
   </View>
 )
 
