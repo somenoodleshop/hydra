0743152f2d0f367f35217da23a5c3c0286626dd4
arandombit <a.random.luis@proton.me>
2025-08-26 21:51:04 -0400

    reintroduce model selection dropdown & new chat button

--- src/container/Chat.jsx
+++ src/container/Chat.jsx
@@ -37,7 +37,25 @@ const Chat = ()=> {
         <View className='flex w-full h-[7%]' style={{ border: '1px solid blue' }}></View>
       </View>
       <View className='flex w-[80%]' style={{ border: '1px solid red' }}>
-        <View className='flex w-full h-[7%]' style={{ border: '1px solid blue' }}></View>
+        <View className='flex w-full h-[7%] flex-row items-center justify-between' style={{ border: '1px solid blue' }}>
+          <Select value={model.value} onValueChange={setModel}>
+            <SelectTrigger>
+              <SelectValue>
+                <Text>{ model.label || 'Select a model' }</Text>
+              </SelectValue>
+            </SelectTrigger>
+            <SelectContent>
+              { models.map(m => <SelectItem {...m}><Text>{ m.label }</Text></SelectItem>) }
+            </SelectContent>
+          </Select>
+          <Button
+            variant='outline'
+            className='w-fit mb-4 mt-4'
+            onPress={createNewSession({ sessions, setSessions, setCurrentSession })}
+          >
+            <Text>New Chat</Text>
+          </Button>
+        </View>
         <View className='flex w-full h-[86%]' style={{ border: '1px solid blue' }}></View>
         <View className='flex w-full h-[7%]' style={{ border: '1px solid blue' }}></View>
       </View>
