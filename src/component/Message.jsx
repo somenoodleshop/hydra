7d13a88ca5ae7cbf29af126af8e8b2d69199f330
arandombit <a.random.luis@proton.me>
2025-08-30 23:21:41 -0400

    improve message alignment based on sender role

--- src/component/Message.jsx
+++ src/component/Message.jsx
@@ -3,7 +3,7 @@ import { View } from 'react-native'
 import { Text } from '~/reusables/ui/text'
 
 const Message = props => (
-  <View className='items-center max-w-[80%] px-4 py-2 mb-2 rounded-2xl'>
+  <View className={`${props.role === 'assistant' ? 'items-start' : 'items-end'} max-w-[80%] px-4 py-2 mb-2 rounded-2xl`}>
     <Text>{ props.content }</Text>
   </View>
 )
