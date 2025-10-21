bf2d9981495e9270e57f3761a71b020c9cec33f1
arandombit <a.random.luis@proton.me>
2025-10-20 23:00:00 -0400

    add input element for editing session names

--- src/container/Sessions.jsx
+++ src/container/Sessions.jsx
@@ -1,6 +1,7 @@
 import { View } from 'react-native'
 
 import { Button } from '~/reusables/ui/button'
+import { Input } from '~/reusables/ui/input'
 import { Text } from '~/reusables/ui/text'
 
 import ContextMenu from '~/component/ContextMenu'
@@ -8,16 +9,18 @@ import ContextMenu from '~/component/ContextMenu'
 const Sessions = props => (
   <View style={{ gap: 8 }}>
     { props.sessions.map(session => (
-      <ContextMenu items={props.menu} onEdit={props.onEdit} onDelete={props.onDelete}>
-        <Button
-          key={session.id}
-          variant={session.id === props.currentSession ? 'secondary' : 'ghost'}
-          onPress={() => props.onSessionSelect(session.id)}
-          className='w-full items-start'
-        >
+      props.isEditing ? <Input key={session.id} value={props.value} onChangeText={props.onChange} /> : (
+        <ContextMenu items={props.menu} onEdit={props.onEdit} onDelete={props.onDelete}>
+          <Button
+            key={session.id}
+            variant={session.id === props.currentSession ? 'secondary' : 'ghost'}
+            onPress={() => props.onSessionSelect(session.id)}
+            className='w-full items-start'
+          >
             <Text>{ session.name }</Text>
-        </Button>
-      </ContextMenu>
+          </Button>
+        </ContextMenu>
+      )
     )) }
   </View>
 )
