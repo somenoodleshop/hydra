e6bc2d13a1573ad5776feb0ce9800ca6778efea2
arandombit <a.random.luis@proton.me>
2025-10-26 23:51:11 -0400

    remove unnecessary components from settings screen

--- src/container/Settings.jsx
+++ src/container/Settings.jsx
@@ -7,13 +7,8 @@ const Settings = () => {
   return (
     <View className='flex-1 w-full justify-center items-center gap-5 p-6'>
       <Text>Settings</Text>
-      <View className='space-y-2'>
         <Text className='text-xl font-bold'>Theme</Text>
-        <View className='flex-row items-center justify-between'>
-          <Text>Dark Mode</Text>
-          <ThemeToggle />
-        </View>
-      </View>
+        <ThemeToggle />
     </View>
   )
 }
