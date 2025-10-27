1aec3be8cf5f53c0c81c1f1ae6b1c89349640187
arandombit <a.random.luis@proton.me>
2025-10-27 19:02:09 -0400

    remove superfluous encapsulating view component for settings screen

--- src/app/settings.jsx
+++ src/app/settings.jsx
@@ -3,9 +3,5 @@ import { View } from 'react-native'
 import Settings from '~/container/Settings'
 
 export default function Screen() {
-  return (
-    <View className='flex-1 w-full justify-center items-center gap-5 p-6'>
-      <Settings />
-    </View>
-  )
+  return <Settings />
 } 
