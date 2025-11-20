a4ced916e97a003974f17174aad42eccab4ca3e4
arandombit <a.random.luis@proton.me>
2025-11-19 23:48:25 -0500

    realign items in settings page to top

--- src/container/Settings.jsx
+++ src/container/Settings.jsx
@@ -5,7 +5,7 @@ import { ThemeToggle } from '~/reusables/ThemeToggle'
 
 const Settings = () => {
   return (
-    <View className='flex-1 w-full justify-center items-center gap-5 p-6'>
+    <View className='flex-1 w-full justify-start items-center gap-5 p-6'>
       <Text>Settings</Text>
         <Text className='text-xl font-bold'>Theme</Text>
         <ThemeToggle />
